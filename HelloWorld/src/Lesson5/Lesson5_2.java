522711f5476f4b0c97bd22779992c17b47f2a4fc
satou-0113 <satou-0113@localhost>
2024-04-19 14:59:56 +0900

    Lesson5 (4/19） 15:00

---
+++ HelloWorld/src/Lesson5/Lesson5_2.java
@@ -0,0 +1,72 @@
+package Lesson5;
+
+/**
+ * 
+ * 第5章 実力問題（クラスの基本）
+ * 
+ * 演算子（+、-、*、/）と計算値のフィールドを持つ計算クラスを作成してください。
+ * 演算子と計算値を入力し、計算結果を出力してください。
+ * 
+ */
+
+import java.util.Scanner;
+
+class Lesson5_2 {
+
+	/**
+	 * 
+	 * @parm num コマンドの引数　出力される文字列、数値を指定
+	 * @String x num1とnum2 の計算結果
+	 * @return 対象外の数値を入力するとやり直し
+	 */
+	
+	double num1;
+	double num2;
+	String Z;
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner =
+				new Scanner(System.in);
+		
+		System.out.println("1つ目の数値を入力してください。");
+		double num1 = scanner.nextDouble();
+		
+		System.out.println("2つ目の数値を入力してください。");
+		double num2 = scanner.nextDouble();
+		
+		System .out .println("演算子を入力してください。");
+		String x = scanner.next();
+		
+		double Z = 0.0;
+		switch(x) {
+		case "+":
+			Z = num1 + num2;
+			break;
+		
+		case "-":
+			Z = num1 - num2;
+			break;
+		
+		case "*":
+			Z = num1 * num2;
+			break;
+			
+		case "/":
+			Z = num1 / num2;
+			if(num2 != 0) {
+				Z = num1 / num2;
+			} else {
+				System.out.println("入力した数値は割り切れません。");
+				return;
+			}
+			break;
+			
+		default:
+            System.out.println("無効な演算子です。");
+            return; // プログラムを終了
+			}
+		
+		System.out.println("計算結果:" + Z);
+	}
+}
