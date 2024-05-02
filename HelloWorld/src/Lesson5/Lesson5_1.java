85753e33484a5bfd03ff185a5186b3d363df7dbc
satou-0113 <satou-0113@localhost>
2024-05-02 21:00:27 +0900

    Lesson5 (5/2) 21:00

--- HelloWorld/src/Lesson5/Lesson5_1.java
+++ HelloWorld/src/Lesson5/Lesson5_1.java
@@ -1,7 +1,5 @@
 package Lesson5;
-
 import java.util.Scanner;
-
 /**
  * 
  * 第5章 実力問題（クラスの基本）
@@ -10,60 +8,59 @@ import java.util.Scanner;
  * 商品名と金額を入力し、計算結果を出力してください。
  *
  */
-
-class Lesson5_1 {
+class Lesson5_1
+{
     String name;
     double price;
     double TAX_RATE = 0.1;
  /**
-  * 
   * @String name  コマンドの引数　出力される文字列、数値を指定（商品名）
   * @param price  コマンドの引数　出力される文字列、数値を指定（価格）
   * @param getTotalPrice 商品の合計金額（税込み価格）を計算
   * @TAX_RATE 消費税率（10％）
   * @return 対象外の数値を入力するとやり直し
-  * 
   */
-
-    public Lesson5_1(String name, double price) {
+    public Lesson5_1(String name, double price)
+    {
         this.name = name;
         this.price = price;
     }
-
-    public double getTotalPrice() {
+    public double getTotalPrice()
+    {
         return price + (price * TAX_RATE);
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner =
         		new Scanner(System.in);
-
         System.out.println("商品名を入力してください:");
         String name = scanner.nextLine();
-
         double price;
-        while (true) {
+        while (true)
+        {
         System.out.println("金額を入力してください。");
-        
-        if(scanner.hasNextDouble()) {
+        if(scanner.hasNextDouble())
+        {
             price = scanner.nextDouble();
-            
-            if(price < 0 ) {
+            if(price < 0 )
+            {
             	System.out.println("0以上の金額を入力してください。");
             	return;
-            	} else {
-            		break;
-            	}
-            } else {
-            	System.out.println("数値を入力してください。");
-                scanner.next(); // 不正な入力を読み捨てる
+            }
+            else
+            {
+            	break;
             }
         }
-    
+        else
+        {
+        	System.out.println("数値を入力してください。");
+        	scanner.next(); // 不正な入力を読み捨てる
+        }
+        }
         Lesson5_1 product = new Lesson5_1(name, price);
         double totalPrice = product.getTotalPrice();
-
         System.out.println("購入額: " + totalPrice + "円");
         scanner.close();
-        }
+    }
 }
--- HelloWorld/src/Lesson5/Lesson5_2.java
+++ HelloWorld/src/Lesson5/Lesson5_2.java
@@ -1,72 +1,75 @@
 package Lesson5;
-
 /**
- * 
  * 第5章 実力問題（クラスの基本）
- * 
  * 演算子（+、-、*、/）と計算値のフィールドを持つ計算クラスを作成してください。
  * 演算子と計算値を入力し、計算結果を出力してください。
- * 
  */
-
 import java.util.Scanner;
-
-class Lesson5_2 {
-
+class Lesson5_2
+{
 	/**
-	 * 
 	 * @parm num コマンドの引数　出力される文字列、数値を指定
-	 * @String x num1とnum2 の計算結果
+	 * @String Z num1とnum2 の計算結果
 	 * @return 対象外の数値を入力するとやり直し
 	 */
-	
-	double num1;
-	double num2;
-	String Z;
-	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)
+	{
 		Scanner scanner =
-				new Scanner(System.in);
-		
+				new Scanner(System.in);	
 		System.out.println("1つ目の数値を入力してください。");
 		double num1 = scanner.nextDouble();
-		
 		System.out.println("2つ目の数値を入力してください。");
 		double num2 = scanner.nextDouble();
-		
 		System .out .println("演算子を入力してください。");
 		String x = scanner.next();
-		
+		keisann shiki = new keisann(num1,num2,x);
+		shiki.x();
+	}
+}
+class keisann
+{
+	/**
+	 * @parm num コマンドの引数　出力される文字列、数値を指定
+	 * @String Z num1とnum2 の計算結果
+	 * @return 対象外の数値を入力するとやり直し
+	 */
+	double num1;
+	double num2;
+	String x;
+	public keisann(double num1, double num2, String x)
+	{
+		this.num1 = num1;
+		this.num2 = num2;
+		this.x = x;
+	}
+	public void x()
+	{
 		double Z = 0.0;
-		switch(x) {
+		switch(x)
+		{
 		case "+":
 			Z = num1 + num2;
 			break;
-		
 		case "-":
 			Z = num1 - num2;
 			break;
-		
 		case "*":
 			Z = num1 * num2;
 			break;
-			
 		case "/":
 			Z = num1 / num2;
-			if(num2 != 0) {
+			if(num2 != 0)
+			{
 				Z = num1 / num2;
-			} else {
+			}
+			else
+			{
 				System.out.println("入力した数値は割り切れません。");
-				return;
 			}
 			break;
-			
 		default:
             System.out.println("無効な演算子です。");
-            return; // プログラムを終了
-			}
-		
+		}
 		System.out.println("計算結果:" + Z);
 	}
 }
