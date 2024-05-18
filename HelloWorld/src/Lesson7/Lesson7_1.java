1bef66984a4da8973a9d9c35fdec8aa04b28f9ee
satou-0113 <satou-0113@localhost>
2024-05-18 10:46:53 +0900

    5/18 10:46

--- HelloWorld/src/Lesson7/Lesson7_1.java
+++ HelloWorld/src/Lesson7/Lesson7_1.java
@@ -19,15 +19,15 @@ public class Lesson7_1 {
 		System.out.println("日付を入力してください。（例:yyyy-MM-dd)");
 		String inputDateStr = scanner.nextLine();
 		/** 日付文字列をLocalDateオブジェクトに格納 */
-		 LocalDate inputDate = LocalDate.parse(inputDateStr);
-		 /** 入力した日付に1ヶ月後を加える */
-		 LocalDate nextMonthDate = inputDate.plusMonths(1);
-		 /** 入力されたフォーマット */
-		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		 /** 結果の出力 */
-		 System.out.println("入力された日付は、" + inputDate.format(formatter));
-		 System.out.println("1ヶ月後の日付は、" + nextMonthDate.format(formatter));
-		 /** スキャナーを閉局 */
-		 scanner.close();
+		LocalDate inputDate = LocalDate.parse(inputDateStr);
+		/** 入力した日付に1ヶ月後を加える */
+		LocalDate nextMonthDate = inputDate.plusMonths(1);
+		/** 入力されたフォーマット */
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		/** 結果の出力 */
+		System.out.println("入力された日付は、" + inputDate.format(formatter));
+		System.out.println("1ヶ月後の日付は、" + nextMonthDate.format(formatter));
+		/** スキャナーを閉局 */
+		scanner.close();
 	}
 }
